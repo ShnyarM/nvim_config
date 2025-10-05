ee4e381107c4382ccea428d5ff6bd4e46ea39d64
ShnyarM <shnyar@hotmail.ch>
2025-10-05 20:36:39 +0200

    some changes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -21,6 +21,26 @@ return {
             vim.lsp.config('ts_ls', {})
             vim.lsp.enable('ts_ls')
 
+            vim.lsp.config['ocamllsp'] = {
+                cmd = { '/home/shnyar/.opam/4.14.2/bin/ocamllsp' },
+                filetypes = {
+                    'ocaml',
+                    'ocaml.interface',
+                    'ocaml.menhir',
+                    'ocaml.ocamllex',
+                    'dune',
+                    'reason'
+                },
+                root_markers = {
+                    { 'dune-project', 'dune-workspace' },
+                    { "*.opam", "esy.json", "package.json" },
+                    '.git'
+                },
+                settings = {},
+            }
+
+            vim.lsp.enable('ocamllsp')
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
